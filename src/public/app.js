3bae79c7daef39df868d457460e507e4c65dc39c
ludvigch <ludvigch@kth.se>
2024-10-02 11:37:44 +0200

    fix typo by mac os :(

--- src/public/app.js
+++ src/public/app.js
@@ -1,13 +1,13 @@
  
-  const express = require("express");
-  const cool_package = require("cool_package");
-  const app = express();
-  const PORT = 1337;
-  
-  app.get("/", (req, res) => {
-    res.send("Chopsticks are better than spoons! 🥢");
-  });
-  
-  app.listen(PORT, ()=>{
-    console.log("I want chopsticks");
-  });
+const express = require("express");
+const cool_package = require("cool_package");
+const app = express();
+const PORT = 1337;
+
+app.get("/", (req, res) => {
+  res.send("Chopsticks are better than spoons! 🥢");
+});
+
+app.listen(PORT, ()=>{
+  console.log("I want chopsticks");
+});
